b1e78ab2f75f572368700fdb70a3157aaf669275
henrycgbaker <henry.c.g.baker@hotmail.com>
2025-04-16 13:33:15 +0200

    cleaned up old 'DELETE' files

--- MAIN_run_experimental_suite.py
+++ MAIN_run_experimental_suite.py
@@ -69,7 +69,7 @@ def run_suite(config_list, suite_name):
 
 
 huggingface_models = [
-    #"TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    "TinyLlama/TinyLlama-1.1B-Chat-v1.0",
     "meta-llama/Llama-3.2-1B",
     "meta-llama/Llama-3.2-3B",
     #"meta-llama/Llama-3.1-8B",
